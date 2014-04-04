f9c461ea3d08cf8ffe51d6dcb129a948c00ff0f8
Simon Erhardt <me@rootlogin.ch>
2014-04-04 15:16:01 +0200

    extended installer

--- src/installer.sh
+++ src/installer.sh
@@ -8,16 +8,33 @@ if [ -f "/src/app.conf" ]; then
     ln -s /etc/apache2/sites-available/app.conf /etc/apache2/sites-enabled/app.conf
 fi
 
+SECURITY_FILE=/etc/apache2/conf-available/security.conf
+if [ -f $SECURITY_FILE ]; then
+
+    sed -i \
+        -e "s:ServerTokens OS:ServerTokens Minimal:g" \
+        -e "s:ServerSignature On:ServerSignature Off:g" \
+        $SECURITY_FILE
+
+    echo "<Directory />" >> $SECURITY_FILE
+    echo "AllowOverride None" >> $SECURITY_FILE
+    echo "Order Deny,Allow" >> $SECURITY_FILE
+    echo "Deny From All" >> $SECURITY_FILE
+    echo "</Directory>" >> $SECURITY_FILE
+fi
+
 if [ ! -d "/var/www" ]; then
     mkdir /var/www
 fi
 
-rm -Rvf /var/www/*
+rm -Rf /var/www/*
 
 if [ -f "/src/index.html" ]; then
     cp /src/index.html /var/www/
     chown www-data:www-data /var/www/index.html
     chmod 0666 /var/www/index.html
 fi
+
+a2enmod rewrite
 
 echo Installation successfully
